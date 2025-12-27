4d7ff75035eb11e1c9d2f0161bbaa01c7cb6e85d
athohck <athohck@gmail.com>
2025-12-27 18:40:42 +0100

    Version 3: Gestionnaire de stock complet

--- main.cpp
+++ main.cpp
@@ -15,10 +15,11 @@ void afficheNom(){
 void menuChoix(){
     std::cout << " 1: Enregistrer un produit + stock dispo " << std::endl;
     std::cout << " 2: Consulter le stock disponibe " << std::endl;
+    std::cout << " 3: Supprimer un produit " << std::endl;
+    std::cout << " 4: Modifier la stock d'un produit " << std::endl;
+    std::cout << " 5: Quitter " << std::endl;
 
 }
-
-
 
 //nP = nomProdsuit sP = stockProduit
 void renseignementProduit(std::string &nP, double &sP){
@@ -75,9 +76,66 @@ void afficherStock() {
     }
 }
 
+//Void qui permet de supprimer un produit du stock 
+void supprimerUnProduit(std::string produitASupprimer){
+    std::ifstream fichierASupprimer("stock.txt"); //Ouvrir et lire stock.txt
+    std::ofstream nouveauFichier("temp.txt");    //Créé un fichier temporaire
 
+    std::string ligne;  //Variable pour stocker une ligne
 
+    while (std::getline(fichierASupprimer, ligne))  //Tant qu'il y a des lignes a lire 
+    {   //Si la ligne ne continet pas le produit a supprimer
+        if (ligne.find(produitASupprimer) == std::string::npos)
+        {
+            nouveauFichier << ligne << "\n"; // On la copie dans temp.txt
 
+        }
+        //Sinon on ne la copie pas on la supprime 
+    }
+
+    //On ferme les deux fichier ouvert 
+    fichierASupprimer.close();
+    nouveauFichier.close();
+
+    //On supprime le (stock.txt) grace a remove
+    remove("stock.txt");
+
+    //On renome (temp.txt) ➡️ (stock.txt)
+    rename("temp.txt", "stock.txt");
+}
+
+//Void qui permet de modifier la quantié d'un produit en stock
+void modificationProduitStock(std::string produitAModifier, double nouvelleQuantite){
+    std::ifstream lectureStock("stock.txt");    //Ouvrir et lire dans ("stock.txt")
+    std::ofstream nouveauFichier("temp.txt");   //Cree un nouveau fichier ("temp.txt")
+
+    std::string ligne; //Variable pour stocker une ligne
+
+    while (std::getline(lectureStock, ligne))
+    {
+        if (ligne.find(produitAModifier) != std::string::npos)
+        {   
+            // SI ON TROUVE le produit
+            // On RÉÉCRIT avec la NOUVELLE quantité
+            nouveauFichier << "|Produit : " << produitAModifier << " Stock : " << nouvelleQuantite << "\n";
+        }
+        else 
+        {
+            // SI ON NE TROUVE PAS le produit
+            // On COPIE la ligne d'origine (sans modification)
+            nouveauFichier << ligne << "\n";
+        }    
+    }
+        //On ferme les deux fichier avec le .close()
+    lectureStock.close();
+    nouveauFichier.close();
+    //On supprime le (stock.txt) grace a remove
+    remove("stock.txt");
+
+    //On renome (temp.txt) ➡️ (stock.txt)
+    rename("temp.txt", "stock.txt");
+    
+}
 
 int main(){
     int choixPourMenu;
@@ -112,41 +170,46 @@ int main(){
         {
             afficherStock();
         }
-        else if (choixPourMenu == 3) {
-            std::cout << " Au revoir\n";
+        else if (choixPourMenu == 3) 
+        {
+            std::string produitASupprimer;  //Variable pour supprimer le stock de l'utilisateur 
+            afficherStock(); //On affiche le stock afin que l'utilisateur vois sont stock
+            std::cout << " Quel produit veux-tu supprimer ? (Entre le nom complet): ";
+            std::getline(std::cin, produitASupprimer);
+
+            supprimerUnProduit(produitASupprimer);
+            
         }
+        else if (choixPourMenu == 4)
+        {
+            std::string nomDuProduit;     //Variable pour modifier le stock de l'utilisateur 
+            double nouvelleQuantite;
+            afficherStock();    //On affiche le stock pour que l'utilisateur vois sont stock
+
+            std::cout << " Entre le nom (Complet) de ton produit pour modifier le stock : ";
+            std::getline(std::cin, nomDuProduit);
+            std::cout << " Nouvelle quantite : ";
+            std::cin >> nouvelleQuantite;
+
+            modificationProduitStock(nomDuProduit, nouvelleQuantite);   //Appelle de la fonction pour modifier le stock
+
+
+
+
+
+        }
+        else if (choixPourMenu == 5)
+        {
+            std::cout << "Au revoir " << std::endl;
+        }
+        
         else {
             std::cout << "❌ Choix invalide !\n";
         }
         
 
         
-    } while (choixPourMenu != 3);
-
-
-
-
-
-
-    
-    
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    } while (choixPourMenu != 5);
 
 
     return 0;
